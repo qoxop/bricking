b3e9f49804f57bedbc7af7e94882626c556970d0
张集伟 <1106291186@qq.com>
2022-09-03 22:34:10 +0800

    补充脚本命令

--- packages/bricking/bin/index.js
+++ packages/bricking/bin/index.js
@@ -28,10 +28,10 @@ require('yargs')
         }
     ).command(
         'install',
-        '安装基础包的对等依赖(以提供开发时的类型提醒)',
+        '安装基建包的对等依赖(以供开发时的类型提醒)',
         () => void 0,
-        () => {
-            require('../dist/install').install();
+        (args) => {
+            require('../dist/install').install((args._ || []).slice(1));
         }
     )
     .help()
