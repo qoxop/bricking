21895480647e03c2c4faebcf2b9853e38ad4bdaa
张集伟 <1106291186@qq.com>
2022-09-07 20:45:22 +0800

    更新发布脚本判断逻辑

--- scripts/release.js
+++ scripts/release.js
@@ -5,11 +5,9 @@ const options = { stdio: 'inherit' };
  * 更新版本
  */
 // npx changeset version
-spawnSync('pnpm', ['changeset', 'version'], options);
-// check status
-const { stdout } = spawnSync('git', ['status'], { encoding: 'utf-8' });
-
-if (!/working tree clean/.test(stdout)) {
+const { stdout } = spawnSync('pnpm', ['changeset', 'version'], {encoding: 'utf-8'});
+console.log(stdout);
+if (stdout.indexOf('No unreleased changesets found, exiting') < 0) {
   // commit
   spawnSync('git', ['add', '.'], options);
   spawnSync('git', ['commit', '-m="update version [skip actions]"', '--no-verify'], options);
