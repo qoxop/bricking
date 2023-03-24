804394ddbfeceda276727412a80c0c31a07b2eee
张集伟 <1106291186@qq.com>
2023-03-24 11:04:42 +0800

    固定babel-runtime版本

--- examples/shared-libs/.brickingrc.ts
+++ examples/shared-libs/.brickingrc.ts
@@ -2,12 +2,15 @@ import { updateOptions } from '@bricking/base-builder';
 
 export default updateOptions({
   publicPath: 'http://localhost:8080/',
+  compile: {
+    useSourceMap: true,
+  },
   bundle: {
     devEntry: './dev.tsx',
     expose: [
       { name: 'antd', path: './src/antd.ts' },
       { name: 'tasks', path: './src/tasks.ts' },
-      { name: 'react-dom/client', path: './src/react-dom-client.ts', subPath: true },
+      { name: 'react-dom/client', path: './src/react-dom-client.ts', isSubLib: true },
     ],
     exposeAll: true,
   }
